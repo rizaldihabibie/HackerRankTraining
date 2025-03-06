07e97b2140dbd3ff02bc1cd5127627cd9e2f30ca
unknown <rizaldihabibie28@gmail.com>
2025-03-06 09:21:31 +0700

    feat: practice LCA in binary tree

---
+++ src/main/java/com/habibie/hackerranktraining/LCAinBinaryTree.java
@@ -0,0 +1,52 @@
+package com.habibie.hackerranktraining;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BinaryNode {
+    int data;
+    BinaryNode left, right;
+
+    BinaryNode(int value) {
+        data = value;
+        left = right = null;
+    }
+}
+
+public class LCAinBinaryTree {
+    // LCA = Lowest Common Ancestor
+
+    public static BinaryNode findLCA(BinaryNode root, int n1, int n2 ) {
+        List<BinaryNode> path1 = new ArrayList<>();
+        List<BinaryNode> path2 = new ArrayList<>();
+
+        if (!findPath(root, path1, n1) || !findPath(root, path2, n2)) return null;
+
+        int i;
+        for (i = 0; i < path1.size() && i<path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                return path1.get(i-1);
+            }
+        }
+
+        return path1.get(i-1);
+
+    }
+
+    public static boolean findPath(BinaryNode root, List<BinaryNode> path, int n) {
+        if(root == null) {
+            return false;
+        }
+
+        path.add(root);
+        if (root.data == n ||
+                findPath(root.left, path, n) ||
+                findPath(root.right, path, n)
+        ) {
+            return true;
+        }
+
+        path.remove(path.size() -1);
+        return false;
+    }
+}
